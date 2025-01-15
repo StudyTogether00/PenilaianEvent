e41d846903a8e3b41689a73d47bbd9b3b9f064c4
AdityaNurHaki36 <138609711+AdityaNurHaki36@users.noreply.github.com>
2025-01-15 22:56:53 +0700

    up nilai

--- resources/views/pages/Process/Nilai.blade.php
+++ resources/views/pages/Process/Nilai.blade.php
@@ -5,31 +5,35 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                    <div class="material-datatables">
-                        <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0"
-                            width="100%" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th colspan="10">
-                                        <x-button type="button" class="btn-outline-success" icon="fa fa-plus"
-                                            label="Add" onclick="Add()" />
-                                        <x-button type="button" class="btn-outline-info" icon="fa fa-refresh"
-                                            label="Refresh" onclick="Refresh()" />
-                                    </th>
-                                </tr>
-                                <tr>
-                                    <th class="text-center align-middle" rowspan="2">No</th>
-                                    <th class="text-center align-middle" colspan="3">Data Peserta</th>
-                                    <th class="text-center align-middle" rowspan="2">Status</th>
-                                    <th class="disabled-sorting text-center align-middle" rowspan="2">Actions</th>
-                                </tr>
-                                <tr>
-                                    <th class="text-center">Nama</th>
-                                    <th class="text-center">Username</th>
-                                    <th class="text-center">Email</th>
-                                </tr>
-                            </thead>
-                        </table>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <x-form-group type="select" class="col-sm-12 col-md-6" label="Event" name="kd_event"
+                                onchange="Refresh()">
+                                <option value="" disabled>--Choose Event--</option>
+                            </x-form-group>
+                        </div>
+                        <div class="material-datatables col-sm-12">
+                            <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0"
+                                width="100%" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th colspan="10">
+                                            <x-button type="button" class="btn-outline-success" icon="fa fa-plus"
+                                                label="Add" onclick="Add()" />
+                                            <x-button type="button" class="btn-outline-info" icon="fa fa-refresh"
+                                                label="Refresh" onclick="Refresh()" />
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th class="text-center">No</th>
+                                        <th class="text-center">Nama</th>
+                                        <th class="text-center">Username</th>
+                                        <th class="text-center">Nilai Rata2</th>
+                                        <th class="text-center">Action</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -40,21 +44,11 @@
     <x-modal-form id="AddEditData" title="labelAddEdit">
         <div class="modal-body">
             <div class="row">
-                <x-form-group class="col-sm-12 col-md-12" label="Nama Peserta" name="nm_peserta" required />
-                <x-form-group type="select" class="col-sm-12 col-md-12" label="Status" name="jns_kel" required>
-                    <option value="" disabled>--Choose JK--</option>
-                    <option value="1">Laki-Laki</option>
-                    <option value="0">Perempuan</option>
-                </x-form-group>
-                <x-form-group type="date" class="col-sm-12 col-md-12" label="Tanggal Lahir" name="tgl_lhr" required />
-                <x-form-group class="col-sm-12 col-md-12" label="Alamat" name="alamat" required />
-                <x-form-group class="col-sm-12 col-md-12" label="Email" name="email" required />
-                <x-form-group class="col-sm-12 col-md-12" label="Username" name="username" required />
-                <x-form-group type="password" class="col-sm-12 col-md-12" label="Password" name="password" required />
-                <x-form-group type="select" class="col-sm-12 col-md-12" label="Status" name="flag_active" required>
-                    <option value="" disabled>--Choose Status--</option>
-                    <option value="1">Aktif</option>
-                    <option value="0">No Aktif</option>
+                <x-form-group class="col-sm-12 col-md-12" label="No Register" name="no_event" required />
+                <x-form-group type="date" class="col-sm-12 col-md-12" label="Tanggal Register" name="tgl_register"
+                    required />
+                <x-form-group type="select" class="col-sm-12 col-md-12" label="Peserta" name="kd_peserta" required>
+                    <option value="" disabled>--Choose Peserta--</option>
                 </x-form-group>
             </div>
         </div>
@@ -81,41 +75,71 @@
     <script type="text/javascript">
         let table, id_tbl = "#datatables";
         let processData = {};
+        let kd_event = "";
+
+        DataEvent = function() {
+            SendAjax({
+                url: $apiUrl + "MasterData/Event/Active",
+            }, function(result) {
+                let html = "";
+                $.each(result.data, function(index, value) {
+                    html += '<option value="' + value.kd_event + '">' + value.nm_event +
+                        '</option>';
+                });
+                if (html != "") {
+                    $(html).insertAfter("[name='kd_event'] option:first");
+                    $(".selectpicker").selectpicker('refresh');
+                }
+                $("[name='kd_event']").val("").change();
+            });
+        }
 
         Refresh = function() {
+            kd_event = $("[name='kd_event']").val();
             if (!$.fn.DataTable.isDataTable(id_tbl)) {
                 let dtu = {
                     id: id_tbl,
                     data: {
-                        url: $apiUrl + "MasterData/Peserta/List"
+                        url: $apiUrl + "Process/Register/List",
+                        param: function() {
+                            var d = {};
+                            d.kd_event = kd_event;
+                            return JSON.stringify(d);
+                        }
                     }
                 };
                 table = PDataTables(dtu, [{
                     "data": null,
                     "className": "text-center",
                     "render": function(data, type, row, meta) {
-                        return meta.row + meta.settings._iDisplayStart + 1;
+                        return meta.row /*+ meta.settings._iDisplayStart*/ + 1;
+                    }
+                }, {
+                    "data": "no_event",
+                }, {
+                    "data": "tgl_register",
+                    "className": "text-center",
+                    render: function(data, type, row, meta) {
+                        return Convertyyyymmmddd(data);
                     }
                 }, {
                     "data": "nm_peserta",
                 }, {
-                    "data": "username",
-                }, {
-                    "data": "email",
-                }, {
-                    "data": "flag_active",
+                    "data": "jns_kel",
                     render: function(data, type, row, meta) {
                         return data == 1 ? "Laki-Laki" : "Perempuan";
                     }
+                }, {
+                    "data": "email",
                 }, {
                     "data": null,
                     "orderable": false,
                     "className": "text-center",
                     render: function(data, type, row, meta) {
                         let html = "";
-                        html += btnDataTable("Edit Peserta", "btn-outline-primary edit",
+                        html += btnDataTable("Edit Register", "btn-outline-primary edit",
                             "fa fa-edit btn-outline-primary", true);
-                        html += btnDataTable("Delete Peserta", "btn-outline-danger delete",
+                        html += btnDataTable("Delete Register", "btn-outline-danger delete",
                             "fa fa-trash btn-outline-danger");
                         return html;
                     }
@@ -129,9 +153,10 @@
                     $tr = $(this).closest('tr');
                     var data = table.row($tr).data();
                     processData = {
+                        kd_event: data.kd_event,
                         kd_peserta: data.kd_peserta
                     };
-                    $("#FDelData p").html("Are you sure to delete data Peserta <b>" + data.nm_peserta +
+                    $("#FDelData p").html("Are you sure to delete data register <b>" + data.nm_peserta +
                         "</b> ?");
                     ShowModal("MDelData");
                 });
@@ -141,34 +166,58 @@
         }
 
         Add = function() {
-            ShowData();
+            if (kd_event == "" || kd_event == null || kd_event == undefined) {
+                MessageNotif("Silahkan Pilih Event Terlebih Dahulu!", "warning");
+            } else {
+                ShowData();
+            }
         }
 
         ShowData = function(act = "Add", data = "") {
             let form_id = "#FAddEditData";
-            $("h4[labelAddEdit]").text(act + " Data Peserta");
+            $("h4[labelAddEdit]").text(act + " Data Register");
             processData = {
                 action: act,
-                kd_peserta: (act == "Add" ? 0 : data.kd_peserta),
-                nm_peserta: (act == "Add" ? "" : data.nm_peserta),
-                jns_kel: (act == "Add" ? "" : data.jns_kel),
-                tgl_lhr: (act == "Add" ? moment(new Date()).format("YYYY-MM-DD") : data.tgl_lhr),
-                alamat: (act == "Add" ? "" : data.alamat),
-                email: (act == "Add" ? "" : data.email),
-                username: (act == "Add" ? "" : data.username),
-                password: (act == "Add" ? "" : data.password),
-                flag_active: (act == "Add" ? "" : data.flag_active),
+                kd_event: kd_event,
+                no_event: (act == "Add" ? "" : data.no_event),
+                tgl_register: (act == "Add" ? moment(new Date()).format("YYYY-MM-DD") : data.tgl_register),
+                kd_peserta: (act == "Add" ? "" : data.kd_peserta),
             };
-            $(form_id + " [name='nm_peserta']").val(processData.nm_peserta).change();
-            $(form_id + " [name='jns_kel']").val(processData.jns_kel).change();
-            $(form_id + " [name='tgl_lhr']").val(processData.tgl_lhr).change();
-            $(form_id + " [name='alamat']").val(processData.alamat).change();
-            $(form_id + " [name='email']").val(processData.email).change();
-            $(form_id + " [name='username']").val(processData.username).change();
-            $(form_id + " [name='password']").val(processData.password).change();
-            $(form_id + " [name='flag_active']").val(processData.flag_active).change();
-            $(form_id).parsley().reset();
-            ShowModal("MAddEditData");
+            $(form_id + " [name='no_event']").val(processData.no_event).change();
+            $(form_id + " [name='tgl_register']").val(processData.tgl_register).change();
+            $(form_id + " [name='kd_peserta']").removeAttr('disabled').find('option:not(:first)').remove().end();
+            if (processData.action == "Add") {
+                Loader("show");
+                // Get Data Peserta Ready
+                SendAjax({
+                    url: $apiUrl + "Process/Register/PesertaReady",
+                    param: {
+                        kd_event: processData.kd_event
+                    }
+                }, function(result) {
+                    let html = "";
+                    $.each(result.data, function(index, value) {
+                        html += '<option value="' + value.kd_peserta + '">' + value.nm_peserta +
+                            '</option>';
+                    });
+                    if (html != "") {
+                        $(html).insertAfter(form_id + " [name = 'kd_peserta'] option:first");
+                        $(form_id + " .selectpicker").selectpicker('refresh');
+                    }
+                    $(form_id + " [name='kd_peserta']").val(processData.kd_peserta).change();
+                    $(form_id).parsley().reset();
+                    ShowModal("MAddEditData");
+                }, function() {
+                    Loader();
+                });
+            } else {
+                let html = '<option value="' + data.kd_peserta + '">' + data.nm_peserta + '</option>';
+                $(html).insertAfter(form_id + " [name = 'kd_peserta'] option:first");
+                $(form_id + " .selectpicker").selectpicker('refresh');
+                $(form_id + " [name='kd_peserta']").attr('disabled', true).val(processData.kd_peserta).change();
+                $(form_id).parsley().reset();
+                ShowModal("MAddEditData");
+            }
         }
 
         Save = function() {
@@ -176,17 +225,12 @@
             if ($(form_id).parsley().validate()) {
                 Loader("show");
                 // Pass Data To Object
-                processData.nm_peserta = $(form_id + " [name='nm_peserta']").val();
-                processData.jns_kel = $(form_id + " [name='jns_kel']").val();
-                processData.tgl_lhr = $(form_id + " [name='tgl_lhr']").val();
-                processData.alamat = $(form_id + " [name='alamat']").val();
-                processData.email = $(form_id + " [name='email']").val();
-                processData.username = $(form_id + " [name='username']").val();
-                processData.password = $(form_id + " [name='password']").val();
-                processData.flag_active = $(form_id + " [name='flag_active']").val();
+                processData.no_event = $(form_id + " [name='no_event']").val();
+                processData.tgl_register = $(form_id + " [name='tgl_register']").val();
+                processData.kd_peserta = $(form_id + " [name='kd_peserta']").val();
                 //Setup Send Ajax
                 let data = {
-                    url: $apiUrl + "MasterData/Peserta/Save",
+                    url: $apiUrl + "Process/Register/Save",
                     param: processData
                 };
                 // Process Ajax
@@ -203,7 +247,7 @@
         Delete = function() {
             Loader("show");
             let data = {
-                url: $apiUrl + "MasterData/Peserta/Delete",
+                url: $apiUrl + "Process/Register/Delete",
                 param: processData
             };
             SendAjax(data, function(result) {
@@ -216,6 +260,7 @@
         }
 
         $(document).ready(function() {
+            DataEvent();
             Refresh();
             LoadPicker();
         });
